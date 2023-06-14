1310210298023b99f95e4210390056aeb012b83f
Anastasia <telnova.anastasia@gmail.com>
2023-06-14 14:32:47 +0300

    first commit

---
+++ eslint-plugin-fixed/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  rules: {
+    'fix': {
+      meta: {
+        fixable: "code"
+      },
+      create(context) {
+        return {
+          // логика правила
+        }
+      },
+    },
+  },
+}
